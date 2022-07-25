af2c86ca370474d9542d777a1e44e24d2bc6d3ba
puffio <51675874+budopod1@users.noreply.github.com>
2022-07-25 20:40:42 +0000

    Zombies damage the player; yes there is a error when the player dies, it will be fixed latter

--- entities/zombie.py
+++ entities/zombie.py
@@ -1,5 +1,7 @@
 from entities.entity import Entity
 from entities.player import Player
+from math import sqrt
+from timer import Cooldown
 
 
 class Zombie(Entity):
@@ -9,7 +11,11 @@ class Zombie(Entity):
         self.health = 5
         self.jump_power = 6
         self.move_power = 3
-        self.detection = 15
+        self.detection_range = 15
+        self.attack_range = 2
+        self.attack_cooldown_length = 2
+        self.attack_cooldown = Cooldown(self.attack_cooldown_length)
+        self.damage_amount = 1.5
         self.collider = [
             (-1, -1),
             (-1, 1),
@@ -26,14 +32,20 @@ class Zombie(Entity):
         if self.target:
             if self.target.destroyed:
                 self.target = None
+                return
 
-            diff = self.target.x - self.x
-            self.xv = diff / abs(diff) * self.move_power
+            diff_x = self.target.x - self.x
+            diff_y = self.target.y - self.y
+            self.xv = diff_x / abs(diff_x) * self.move_power
 
             if self.walled and self.grounded:
                 self.yv = self.jump_power
+
+            if sqrt(diff_x ** 2 + diff_y ** 2) < self.attack_range and self.attack_cooldown.expired():
+                self.target.damage(self.damage_amount)
+                self.attack_cooldown.start(self.attack_cooldown_length)
         else:
-            min_dist = self.detection
+            min_dist = self.detection_range
             target = None
             for entity in self.server.entities:
                 if isinstance(entity, Player):
